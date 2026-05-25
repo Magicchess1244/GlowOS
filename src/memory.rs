ca7ef39ec1d3a4586933a01e3c14e9d5b04547d0
KONICHIGUA BEIBI <196363069+Magicchess1244@users.noreply.github.com>
2026-05-25 15:41:15 +0200

    Basic DMA(Contiguous mem)

--- src/memory.rs
+++ src/memory.rs
@@ -1,17 +1,14 @@
 use x86_64::{
     PhysAddr, VirtAddr,
     structures::paging::{
-        FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PhysFrame, Size4KiB,
+        FrameAllocator, FrameDeallocator, Mapper, OffsetPageTable, Page, PageTable, PhysFrame, Size4KiB, PageTableFlags
     },
 };
-
 use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
-    unsafe {
-        let level_4_table = active_level_4_table(physical_memory_offset);
-        OffsetPageTable::new(level_4_table, physical_memory_offset)
-    }
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
 }
 
 unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
@@ -23,27 +20,71 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut
     let virt = physical_memory_offset + phys.as_u64();
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
-    unsafe { &mut *page_table_ptr }
+    &mut *page_table_ptr
+}
+
+pub fn map_xhci_dma_region(
+    page: Page,
+    frame: PhysFrame,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    let flags = PageTableFlags::PRESENT 
+              | PageTableFlags::WRITABLE 
+              | PageTableFlags::NO_CACHE 
+              | PageTableFlags::WRITE_THROUGH;
+
+    let map_to_result = unsafe {
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("xHCI DMA mapping failed").flush();
 }
 
 pub fn create_example_mapping(
     page: Page,
+    frame: PhysFrame,
     mapper: &mut OffsetPageTable,
     frame_allocator: &mut impl FrameAllocator<Size4KiB>,
 ) {
-    use x86_64::structures::paging::PageTableFlags as Flags;
-
-    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
-    let flags = Flags::PRESENT | Flags::WRITABLE;
+    let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
 
     let map_to_result = unsafe {
-        // FIXME: this is not safe, we do it only for testing
         mapper.map_to(page, frame, flags, frame_allocator)
     };
     map_to_result.expect("map_to failed").flush();
 }
 
 pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
+
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
+
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        let usable_regions = regions
+            .filter(|r| r.region_type == MemoryRegionType::Usable);
+        let addr_ranges = usable_regions
+            .map(|r| r.range.start_addr()..r.range.end_addr());
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
 
 unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
@@ -53,31 +94,90 @@ unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
     }
 }
 
-pub struct BootInfoFrameAllocator {
-    memory_map: &'static MemoryMap,
-    next: usize,
+pub struct BitmapFrameAllocator {
+    bitmap: &'static mut [u8],
+    total_frames: usize,
 }
 
+impl BitmapFrameAllocator {
+    pub unsafe fn new(bitmap_storage: &'static mut [u8], total_frames: usize) -> Self {
+        BitmapFrameAllocator {
+            bitmap: bitmap_storage,
+            total_frames,
+        }
+    }
 
-impl BootInfoFrameAllocator {
-    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
-        BootInfoFrameAllocator {
-            memory_map,
-            next: 0,
+    fn is_allocated(&self, frame_idx: usize) -> bool {
+        let byte_idx = frame_idx / 8;
+        let bit_idx = frame_idx % 8;
+        (self.bitmap[byte_idx] & (1 << bit_idx)) != 0
+    }
+
+    fn set_range(&mut self, start_idx: usize, count: usize, allocated: bool) {
+        for i in 0..count {
+            let idx = start_idx + i;
+            let byte_idx = idx / 8;
+            let bit_idx = idx % 8;
+            if allocated {
+                self.bitmap[byte_idx] |= 1 << bit_idx;
+            } else {
+                self.bitmap[byte_idx] &= !(1 << bit_idx);
+            }
+        }
+    }
+
+    pub fn allocate_contiguous(&mut self, count: usize) -> Option<PhysFrame> {
+        let mut continuous_found = 0;
+        let mut start_idx = 0;
+
+        for frame_idx in 0..self.total_frames {
+            if !self.is_allocated(frame_idx) {
+                if continuous_found == 0 {
+                    start_idx = frame_idx;
+                }
+                continuous_found += 1;
+
+                if continuous_found == count {
+                    self.set_range(start_idx, count, true);
+                    let phys_addr = PhysAddr::new((start_idx * 4096) as u64);
+                    return Some(PhysFrame::containing_address(phys_addr));
+                }
+            } else {
+                continuous_found = 0;
+            }
+        }
+        None
+    }
+
+    pub fn fill_from_boot_allocator(&mut self, boot_alloc: &BootInfoFrameAllocator) {
+        self.bitmap.fill(0xFF);
+
+        for frame in boot_alloc.usable_frames() {
+            let frame_idx = (frame.start_address().as_u64() / 4096) as usize;
+            if frame_idx < self.total_frames {
+                let byte_idx = frame_idx / 8;
+                let bit_idx = frame_idx % 8;
+                self.bitmap[byte_idx] &= !(1 << bit_idx);
+            }
+        }
+
+        for frame_idx in 0..boot_alloc.next {
+            let byte_idx = frame_idx / 8;
+            let bit_idx = frame_idx % 8;
+            self.bitmap[byte_idx] |= 1 << bit_idx;
         }
     }
 }
 
+unsafe impl FrameAllocator<Size4KiB> for BitmapFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        self.allocate_contiguous(1)
+    }
+}
 
-impl BootInfoFrameAllocator {
-    /// Returns an iterator over the usable frames specified in the memory map.
-    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
-        let regions = self.memory_map.iter();
-        let usable_regions = regions
-            .filter(|r| r.region_type == MemoryRegionType::Usable);
-        let addr_ranges = usable_regions
-            .map(|r| r.range.start_addr()..r.range.end_addr());
-        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
-        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+impl FrameDeallocator<Size4KiB> for BitmapFrameAllocator {
+    unsafe fn deallocate_frame(&mut self, frame: PhysFrame) {
+        let frame_idx = (frame.start_address().as_u64() / 4096) as usize;
+        self.set_range(frame_idx, 1, false);
     }
 }
